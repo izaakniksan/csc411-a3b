e1da6070f01640c1cfa639f546b9cc0df8f072c7
izaakniksan <izaakniksan@gmail.com>
2018-03-21 00:13:58 -0400

    minor change

--- Code/part1.py
+++ Code/part1.py
@@ -139,10 +139,7 @@ def main():
     y = [0, 1]
     clf = svm.SVC()
     clf.fit(X, y)  
-    SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
-        decision_function_shape='ovr', degree=3, gamma='auto', kernel='rbf',
-        max_iter=-1, probability=False, random_state=None, shrinking=True,
-        tol=0.001, verbose=False)
+
     
     
     
